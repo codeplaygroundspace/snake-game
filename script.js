db7ec87a66331bf574d5e2c1ef38219955ac2724
codeplaygroundspace <rosinaaa@gmail.com>
2024-11-21 12:22:54 +0000

    initial commit game setup

---
+++ script.js
@@ -0,0 +1,57 @@
+const gameBoard = document.querySelector("#gameBoard");
+const ctx = gameBoard.getContext("2d");
+const scoreText = document.querySelector("#scoreText");
+const resetBtn = document.querySelector("#resetBtn");
+const gameWidth = gameBoard.width;
+const gameHeight = gameBoard.height;
+const boardBackground = "white";
+const snakeColour = "lightgreen";
+const snakeBorder = "black";
+const foodColour = "red";
+const unitSize = 25;
+let running = false; // check if game is running or not
+
+let xVelocity = unitSize;
+let yVelocity = 0;
+let foodX;
+let foodY;
+let score = 0;
+//Snake
+let snake = [
+  {
+    x: unitSize * 4,
+    y: 0,
+  },
+  {
+    x: unitSize * 3,
+    y: 0,
+  },
+  {
+    x: unitSize * 2,
+    y: 0,
+  },
+  {
+    x: unitSize,
+    y: 0,
+  },
+  {
+    x: 0,
+    y: 0,
+  },
+];
+window.addEventListener("keydown", changeDirection);
+resetBtn.addEventListener("click", resetGame);
+gameStart();
+
+// Functions
+function gameStart() {}
+function nextTick() {}
+function clearBoard() {}
+function createFood() {}
+function drawFood() {}
+function moveSnake() {}
+function drawSnake() {}
+function changeDirection() {}
+function checkGameOver() {}
+function displayGameOver() {}
+function resetGame() {}
